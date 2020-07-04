7c88fc4d695e3c362debf5521e8cb176e94abf2a
madhurapabu <madhurapabu@gmail.com>
2020-07-04 13:34:23 +0530

    Updated UI

--- src/components/sidebar/Sidebar.svelte
+++ src/components/sidebar/Sidebar.svelte
@@ -20,20 +20,35 @@
 </script>
 
 <div
-  class="flex flex-col items-center justify-between overflow-hidden {showSidebar ? 'w-64' : 'w-12'}
-  h-full bg-primary transition-all duration-300 ease-in-out fixed">
-  Sidebar
-  <div>
-    {#each menuItems as menuItem}
-      <span
-        class="flex flex-row"
-        on:click={() => {
-          replace(menuItem.path);
-        }}>
-        <img src="./icons/{menuItem.icon}.svg" style="stroke: white;" alt="" />
-        {menuItem.display}
-      </span>
-    {/each}
+  class="flex flex-col overflow-hidden {showSidebar ? 'w-64 ' : 'w-16'} h-full
+  bg-white transition-all duration-300 ease-in-out fixed border-r-2 shadow-2xl
+  border-gray-600">
+  <div class="divide-y divide-white">
+    <div class="py-2">
+      <div>
+        {#each menuItems as menuItem}
+          <span
+            class="flex flex-row py-10 border rounded text-dark border-white
+            hover:text-dark "
+            on:click={() => {
+              replace(menuItem.path);
+            }}>
+            <img
+              class="{showSidebar ? 'px-10 ' : 'px-5 py-5'} hover:bg-green-900
+              bg-2"
+              src="./icons/{menuItem.icon}.svg"
+              style="stroke:white; "
+              alt="" />
+            {menuItem.display}
+          </span>
+        {/each}
+      </div>
+    </div>
   </div>
-  <p on:click={toggleSidebar}>close</p>
+  <p on:click={toggleSidebar}>
+    <img
+      class={showSidebar ? ' px-10 py-64 w-32' : 'px-5 py-32'}
+      src={showSidebar ? './icons/left.svg' : './icons/right.svg'}
+      alt="" />
+  </p>
 </div>
