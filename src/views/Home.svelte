46ced28f3ff22345144d8415a02f30d97d002e73
CharukaHS <charuka@protonmail.com>
2020-07-12 03:29:59 +0530

    Updated home page w/ dummy data

--- src/views/Home.svelte
+++ src/views/Home.svelte
@@ -1,1 +1,34 @@
-<div class="flex flex-col h-full">homeeeeeee</div>
+<script>
+  import MediumCard from "../components/cards/Medium.svelte";
+
+  const dummyData = {
+    initials: "S. S",
+    lname: "Smith",
+    treatment_his: "Clogapine",
+    last_clinic_visit: new Date(),
+    next_clinic_date: new Date(),
+    remarks: "Him gud boi"
+  };
+</script>
+
+<div class="flex flex-col p-4">
+  <h1 class="text-2xl">Today's clinic patients</h1>
+
+  <div class="flex flex-row flex-wrap mt-4">
+    <MediumCard data={dummyData} />
+    <MediumCard data={dummyData} />
+    <MediumCard data={dummyData} />
+    <MediumCard data={dummyData} />
+  </div>
+</div>
+
+<div class="flex flex-col p-4">
+  <h1 class="text-2xl">Scheduled home visits</h1>
+
+  <div class="flex flex-row flex-wrap mt-4">
+    <MediumCard data={dummyData} />
+    <MediumCard data={dummyData} />
+    <MediumCard data={dummyData} />
+    <MediumCard data={dummyData} />
+  </div>
+</div>
