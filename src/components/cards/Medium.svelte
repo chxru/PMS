6a4d171b2cdcd816301c18031e9ab04779e6cb92
CharukaHS <charuka@protonmail.com>
2020-07-11 06:30:02 +0530

    Show search result in cards

---
+++ src/components/cards/Medium.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let data;
+</script>
+
+<div
+  class="flex flex-col h-56 w-84 bg-foreground shadow-md rounded-md px-4 py-2
+  m-2 justify-between items ">
+  <div>
+    <h1 class="text-2xl">{data.lname}</h1>
+    <h3>{data.initials}</h3>
+  </div>
+  <div>
+    <p>Drugs: {data.treatment_his}</p>
+    <p>Last visit: {data.last_clinic_visit}</p>
+    <p>Scheduled next visit: {data.next_clinic_date}</p>
+    <p>Remarks: {data.remarks}</p>
+  </div>
+</div>
--- tailwind.js
+++ tailwind.js
@@ -8,6 +8,11 @@ module.exports = {
         background: "#E9ECF3",
         primary: { ...colors.blue },
       },
+      spacing: {
+        "72": "18rem",
+        "84": "21rem",
+        "96": "24rem",
+      },
     },
   },
   variants: {},
