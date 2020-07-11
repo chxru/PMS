f7c841f19b497143c5d80b8a2a0b155ed9ab96ef
CharukaHS <charuka@protonmail.com>
2020-07-12 02:56:15 +0530

    Add notification panel

---
+++ src/components/cards/Notification.svelte
@@ -0,0 +1,5 @@
+<div
+  class="flex max-w-full box-border mx-2 my-2 px-2 py-4 rounded border
+  border-background">
+  13 Patients missed clinic yesterday
+</div>
--- src/components/topbar/Topbar.svelte
+++ src/components/topbar/Topbar.svelte
@@ -1,10 +1,20 @@
 <script>
   import { replace } from "svelte-spa-router";
 
+  import NotificationCard from "../cards/Notification.svelte";
+
   // toggle dropdown
   let showDropdown = false;
   const toggleDropdown = () => {
+    showNotification = false;
     showDropdown = !showDropdown;
+  };
+
+  // toggle notification
+  let showNotification = false;
+  const toggleNotification = () => {
+    showDropdown = false;
+    showNotification = !showNotification;
   };
 
   // signout
@@ -21,7 +31,12 @@
     <h1 class="text-base">Patient Tracking System</h1>
   </div>
   <div class="flex flex-row items-center">
-    <h1 class="text-base">User</h1>
+    <h1 class="text-base mr-4">User</h1>
+    <img
+      src="./icons/bell.svg"
+      alt="notification"
+      class="ml-2 h-4"
+      on:click={toggleNotification} />
     <img
       src="./icons/chevron-down.svg"
       alt="dropdown"
@@ -32,7 +47,13 @@
 
 <div
   class="flex w-32 bg-foreground absolute right-0 mt-16 mr-4 {showDropdown ? 'visible' : 'hidden'}
-  justify-center py-4 cursor-pointer"
+  justify-center py-4 cursor-pointer rounded-md"
   on:click={signout}>
   Signout
+</div>
+
+<div
+  class="flex flex-col w-96 bg-foreground absolute right-0 mt-16 mr-4 {showNotification ? 'visible' : 'hidden'}
+  justify-center py-4 cursor-pointer rounded-md">
+  <NotificationCard />
 </div>
